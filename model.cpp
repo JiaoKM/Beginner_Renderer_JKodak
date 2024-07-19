1a9f8bdcd82beb5ba3156c81eaa23a0b2d95a959
JKodak <kmjiao_kodak@163.com>
2024-07-20 00:05:59 +0800

    240720update

--- model.cpp
+++ model.cpp
@@ -10,7 +10,7 @@ Model::Model(QString modelPath)
         modelFile.close();
     }
     else {
-        load_flag = false;
+        this->load_flag = false;
         return;
     }
 
@@ -54,8 +54,7 @@ Model::Model(QString modelPath)
             this->faces_.push_back(f);
         }
     }
-
-    load_flag = true;
+    this->load_flag = true;
 }
 
 Model::~Model()
@@ -65,23 +64,29 @@ Model::~Model()
 
 int Model::nVerts()
 {
-    return (int)verts_.size();
+    return (int)this->verts_.size();
 }
 
 int Model::nFaces()
 {
-    return (int)faces_.size();
+    return (int)this->faces_.size();
 }
 
 Vec3f Model::vert(int i)
 {
-    return verts_[i];
+    return this->verts_[i];
 }
 
 std::vector<int> Model::face(int i)
 {
     std::vector<int> face;
-    for (int j = 0; j < (int)faces_[i].size(); j++)
-        face.push_back(faces_[i][j][0]);
+    for (int j = 0; j < (int)this->faces_[i].size(); j++)
+        face.push_back(this->faces_[i][j][0]);
     return face;
+}
+
+Vec2i Model::uv(int iFace, int nVert)
+{
+    int idx = this->faces_[iFace][nVert][1];
+    return Vec2i(this->uv_[idx].x * diffuseMap.width(), this->uv_[idx].y * diffuseMap.height());
 }
