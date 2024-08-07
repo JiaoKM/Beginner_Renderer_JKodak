f9dd9a1d302a61e04c405734ab67b3ec30788a8f
JKodak <kmjiao_kodak@163.com>
2024-08-07 16:28:01 +0800

    240807update

--- shader.cpp
+++ shader.cpp
@@ -1,6 +1,21 @@
 #include "shader.h"
 
-Shader::Shader()
+Shader::Shader(Model *m)
 {
+    this->shader_model = m;
+}
 
+Shader::~Shader()
+{
+    delete this->shader_model;
+}
+
+QRgb Shader::sample2D(const QImage &img, Vec2f uvf)
+{
+    return img.pixel(uvf[0] * img.width(), uvf[1] * img.height());
+}
+
+PhongShader::~PhongShader()
+{
+    delete this->shader_model;
 }
--- shader.h
+++ shader.h
@@ -1,11 +1,31 @@
 #ifndef SHADER_H
 #define SHADER_H
 
+#include <QImage>
+#include <QColor>
+#include "geometry.h"
+#include "model.h"
+
 
 class Shader
 {
 public:
-    Shader();
+    Shader(Model *m);
+    virtual ~Shader();
+    QRgb sample2D(const QImage &img, Vec2f uvf);
+    virtual Vec3f vertexShader(int iFace, int iVert) = 0;
+    virtual QRgb fragmentShader(Vec3f pos) = 0;
+
+    Model *shader_model;
+};
+
+class PhongShader: public Shader
+{
+public:
+    using Shader::Shader;
+    ~PhongShader();
+    Vec3f vertexShader(int iFace, int iVert);
+    QRgb fragmentShader(Vec3f pos);
 };
 
 #endif // SHADER_H
